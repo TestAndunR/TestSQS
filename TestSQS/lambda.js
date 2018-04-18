37dac623165db888ceb54fc43db475e5a7586dd1
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-18 09:35:07 +0530

    TestToolBox

--- TestSQS/lambda.js
+++ TestSQS/lambda.js
@@ -1,45 +1,23 @@
 let AWS = require('aws-sdk');
-let connectionManager = require('./ConnectionManager');
-let SL = require('@slappforge/slappforge-sdk');
-// const rds = new SL.AWS.RDS(connectionManager);
-const sqs = new SL.AWS.SQS(AWS);
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
-	message = event.message
-	sqs.sendMessage({
-		MessageBody: message,
-		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/318300609668/SQS',
-		DelaySeconds: '0',
-		MessageAttributes: {}
-	}, function (data) {	
-		// your logic (logging etc) to handle successful message delivery, should be here
-		console.log("Success");
-	}, function (error) {
-		// your logic (logging etc) to handle failures, should be here
-		console.log("Error");
-	});
-
 	sqs.receiveMessage({
-		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/318300609668/SQS',
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/318300609668/ToolBoxSQS',
 		AttributeNames: ['All'],
-		MaxNumberOfMessages: '5',
+		MaxNumberOfMessages: '1',
 		VisibilityTimeout: '30',
 		WaitTimeSeconds: '0',
 		MessageAttributeNames: ['']
 	}, function (receivedMessages) {
 		receivedMessages.forEach(message => {
-			// your logic to access each message through out the loop. Each message is available under variable message 
-			// within this block
 			console.log(message);
-			callback(null, message);
 		})
 	}, function (error) {
 		// implement error handling logic here
-		console.log("Failed");
-		callback("Error",null);
+		console.log("Error caused");
 	});
 
 
-	// callback(null, 'Successfully executed');
+	callback(null, 'Successfully executed');
 }
-
-
